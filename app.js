79f97cf8027f0b0f337c765f63253aab9dfd289b
Dilina Bandara <dilinaedu@gmail.com>
2023-01-12 00:25:43 +0530

    Add js code

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-const btn = document.querySelector('.btn')
+<input type = 'text' id='input'>
+
+<button id = "magicButton">
+Button
+</button>
+
+const btn = document.getElementById('magicButton')
+let result = document.getElementById('result')
+let input = document.getElementById('input')
+let count = 0
+  
+btn.addEventListener('click', function(){
+count ++
+let num
+let inputNum
+
+inputNum = parseInt(input.value)
+ if(count ==1){
+ num = inputNum + 5
+ input.value = num
+ }
+ else if(count ==2){
+ num = inputNum *5
+ input.value = num
+ }
+ if(count ==3){
+ count =0
+num = inputNum -3
+ input.value = num
+ }
+})
