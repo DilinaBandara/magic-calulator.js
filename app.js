da6764960eeb11e6787e04d2c31064d04ef52eca
Dilina Bandara <dilinaedu@gmail.com>
2023-01-14 01:34:43 +0530

    Tried some new code

--- app.js
+++ app.js
@@ -1,31 +1,16 @@
-<input type = 'text' id='input'>
+const display = document.getElementById('display')
+const reset = document.getElementById('reset-btn')
 
-<button id = "magicButton">
-Button
-</button>
+console.log(display)
+console.log(reset)
 
-const btn = document.getElementById('magicButton')
-let result = document.getElementById('result')
-let input = document.getElementById('input')
-let count = 0
-  
-btn.addEventListener('click', function(){
-count ++
-let num
-let inputNum
+const btns =[]
 
-inputNum = parseInt(input.value)
- if(count ==1){
- num = inputNum + 5
- input.value = num
- }
- else if(count ==2){
- num = inputNum *5
- input.value = num
- }
- if(count ==3){
- count =0
-num = inputNum -3
- input.value = num
- }
-})
+
+for(let i=0; i<10; i++){
+  console.log(btns[i])
+  btns[i] = document.getElementById('btn' + i)
+  btns[i].addEventListener('click',()=>{
+    display.textContent = i
+  })
+}
