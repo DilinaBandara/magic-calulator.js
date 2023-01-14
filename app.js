cd9d1ece3bf99c37a25a543dd59339262f98aa55
Dilina Bandara <dilinaedu@gmail.com>
2023-01-14 11:33:23 +0530

    Complete functionality in calculator

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 const display = document.getElementById('display')
 const reset = document.getElementById('reset-btn')
+const magic = document.getElementById('magic-btn')
 
-console.log(display)
-console.log(reset)
+const buttons = document.querySelectorAll('.btn')
 
-const btns =[]
-
-
-for(let i=0; i<10; i++){
-  console.log(btns[i])
-  btns[i] = document.getElementById('btn' + i)
-  btns[i].addEventListener('click',()=>{
-    display.textContent = i
+buttons.forEach(function(button){
+  button.addEventListener('click', function(){
+    display.value += button.innerText
   })
-}
+})
+
+reset.addEventListener('click', function(){
+  display.value = 0
+})
+
+let count =0
+
+magic.addEventListener('click', function(){
+  count ++
+  let num
+  let displayNum
+
+displayNum =  parseInt(display.value)
+
+  if(count ==1 ){
+  num = displayNum +5
+  display.value = num
+  } else if(count ==2){
+  num = displayNum *2
+  display.value = num
+  } else if(count ==3){
+    count =0
+    num = displayNum -3
+    display.value = num
+  }
+})
